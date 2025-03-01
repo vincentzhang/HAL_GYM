f14bfa0a1d01bee3873322cf94b2fae2e204f55a
VincentZhang <vincent.zhang@ualberta.ca>
2025-03-01 00:11:28 -0700

    fix file pattern in gitignore and correct doc

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="HAL_Gymnasium",
     version="0.0.1",
     install_requires=[
-        "gymnasium==0.29.1",  # This adds gymnasium version 0.28.1 with all extra requirements
+        "gymnasium==0.29.1",  # This adds gymnasium version 0.29.1 with all extra requirements
         "mujoco==2.2.0",          # Adds mujoco version 2.2.0
     ]
 )
