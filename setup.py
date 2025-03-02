756083b320a48818c3f34edf6e676d6a18c7dc5c
VincentZhang <vincent.zhang@ualberta.ca>
2025-03-01 22:13:54 -0700

    bump mujoco version to 3.3.0

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version="0.0.1",
     install_requires=[
         "gymnasium==0.29.1",  # This adds gymnasium version 0.29.1 with all extra requirements
-        "mujoco==2.2.0",          # Adds mujoco version 2.2.0
-    ]
+        "mujoco==3.3.0",  # Adds mujoco version 3.3.0
+    ],
 )
