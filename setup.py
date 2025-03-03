268504b8824da3296b8b0ae5776b8e40a559ba6d
VincentZhang <vincent.zhang@ualberta.ca>
2025-03-02 23:52:31 -0700

    bump gymnasium version and add py package requirement

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="HAL_Gymnasium",
     version="0.0.1",
     install_requires=[
-        "gymnasium==0.29.1",  # This adds gymnasium version 0.29.1 with all extra requirements
+        "gymnasium>=0.29.1",  # This adds gymnasium version 0.29.1 with all extra requirements
         "mujoco==3.3.0",  # Adds mujoco version 3.3.0
     ],
 )
